b1c73f2574817c756c11f284818344781a997921
alekz99 <47984450+alekz99@users.noreply.github.com>
2021-09-28 18:31:00 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,162 @@
+# Oracle APEX Region Plugin - Dropzone
+Dropzone is a region type plugin that allows you to provide nice looking drag’n’drop file uploads.
+It is based on JS Framework dropzone.js (https://github.com/enyo/dropzone).
+
+## Changelog
+#### 1.9.4 - updated dropzone library to 4.3.0 / added a much more detailed upload progress bar per file
+
+#### 1.9.3 - solved performance issues when converting larger files from base64 CLOB to BLOB
+
+#### 1.9.2 - added a random file id to APEX collection (better identification of file by it´s id) / empty file mime types now set to "application/octet-stream" / code cleanup
+
+#### 1.9.1 - added more file preview images / more structured file paths / APEX event triggers on Region (not body) / no global js functions any more, wrapped code in namespace "apexDropzone" / cleaned up js code
+
+#### 1.9 - added option to include a custom callback javascript function which get executed on chosen events (added file & upload completed) + added APEX events for added file & upload completed to trigger Dynamic Actions
+
+#### 1.8 - added option to select a default dropzone style (1: grey border / 2: blue dashed border) / added option to display file type preview images for common file types / minified css/js files (full files for debug mode)
+
+#### 1.7 - solved lots of performance issues (file + base64 data get uploaded to server) / stability improvements
+
+#### 1.6 - added number of parallel uploads option (1 or 2 concurrent) / performance improvements (base64 encoding when file was added instead of sending moment) / better error handling
+
+#### 1.5 - performance improvements(removed redundant AJAX call) / split the clob into a 30k param array (OHS 32k limit for params) / added callback function to apex.server.plugin that processes the files queue
+
+#### 1.4 - added option to refresh a chosen region (with REGION_STATIC_ID) after uploading of all files is complete / Copy&Paste support of images in modern Browsers (like Chrome)
+
+#### 1.3 - set default for "max Files" to dropzone default of 256
+
+#### 1.2 - added max files per upload option / added customizable messages for better multilingual support
+
+#### 1.1 - added options to limit uploading of declared file types (file endings, mime_types, wildcards) / clear dropzone area after uploading of all files finished
+
+#### 1.0 - Initial Release
+
+#### Beta - In development
+
+## Install
+- Import plugin file "region_type_plugin_de_danielh_dropzone.sql.sql" from source directory into your application
+- (Optional) Deploy the CSS/JS files from "server" directory on your webserver and change the "File Prefix" to webservers folder.
+
+## Plugin Settings
+The plugin settings are highly customizable and you can change:
+- **Dropzone Style** - Default style of Dropzone area (grey border / blue dashed border)
+- **Width** - default width of Dropzone area. Valid values are px and % data
+- **Height** - default height of Dropzone area. Valid values are px and % data
+- **Display message** - Enter the text that would be displayed in the Dropzone area.
+- **max. Filesize in MB** - max. file size that is allowed per file. If a file is bigger, it will be removed
+- **Clickable** - If true, the dropzone element itself will be clickable, if false nothing will be clickable
+- **Remove files after upload** - If true, clears all files from Dropzone area if uploading them is finished
+- **max Files** - Maximum number of allowed files that can be uploaded at once
+- **Parallel Uploads** - Number of parallel upload streams to the server (1 or 2 concurrent)
+- **Show common file previews** - Show preview images for common file types when adding files
+- **Accepted file types** - limit uploading of declared file types (file endings, mime_types, wildcards)
+- **Refresh Region after upload (REGION_STATIC_ID)** - Region Static ID of the region which should be refreshed after uploading of all files is complete
+- **Image copy&paste support** - Adds support for Copy&Paste of images in modern Browsers (like Chrome)
+- **Wait in milliseconds** - Wait time between uploaded files in milliseconds
+- **Callback Function Event** - Event of the plugin you want a custom javascript function fired (added file / upload complete)
+- **Callback Function** - A valid javascript function that is executed on the event you selected
+- **File too big message** - Message that is shown when a file is too big
+- **Max files exceeded message** - If max Files is set, this will be the error message when it's exceeded
+- **Page Items to submit** - Page Items that should be set into session state.
+- **PLSQL Code** - PLSQL code which saves the files to database tables or collections
+- **Logging** - Whether to log events in the console
+
+## Plugin Events
+- **Dropzone added file** - DA event that fires when a file gets added to the dropzone area
+- **Dropzone upload completed** - DA event that fires when uploading all files completed
+
+## How to use
+- Create a Dropzone region on target page
+- Choose best fitting plugin attributes (help included)
+
+#### Save to DB using PL/SQL
+For saving files to DB you can use a PL/SQL function like this:
+
+```language-sql
+DECLARE
+  --
+  l_collection_name VARCHAR2(100);
+  l_blob            BLOB;
+  l_filename        VARCHAR2(200);
+  l_mime_type       VARCHAR2(100);
+  l_token           VARCHAR2(32000);
+  l_random_file_id  NUMBER;
+  --
+BEGIN
+  -- get defaults from AJAX Process
+  l_filename  := apex_application.g_x01;
+  l_mime_type := nvl(apex_application.g_x02,
+                     'application/octet-stream');
+  -- random file id
+  l_random_file_id := round(dbms_random.value(100000,
+                                              99999999));
+  -- build BLOB from f01 30k Array (base64 encoded)
+  dbms_lob.createtemporary(l_blob,
+                           FALSE,
+                           dbms_lob.session);
+  FOR i IN 1 .. apex_application.g_f01.count LOOP
+    l_token := wwv_flow.g_f01(i);
+    IF length(l_token) > 0 THEN
+      dbms_lob.append(l_blob,
+                      to_blob(utl_encode.base64_decode(utl_raw.cast_to_raw(l_token))));
+    END IF;
+  END LOOP;
+  --
+  -- create own collection (here starts custom part (for example a Insert statement))
+  -- collection name
+  l_collection_name := 'DROPZONE_UPLOAD';
+  -- check if collection exist
+  IF NOT
+      apex_collection.collection_exists(p_collection_name => l_collection_name) THEN
+    apex_collection.create_collection(l_collection_name);
+  END IF;
+  -- add collection member (only if BLOB not null)
+  IF dbms_lob.getlength(lob_loc => l_blob) IS NOT NULL THEN
+    apex_collection.add_member(p_collection_name => l_collection_name,
+                               p_c001            => l_filename, -- filename
+                               p_c002            => l_mime_type, -- mime_type
+                               p_d001            => SYSDATE, -- date created
+                               p_n001            => l_random_file_id, -- random file id
+                               p_blob001         => l_blob); -- BLOB file content
+  END IF;
+  --
+END;
+```
+
+#### Get files from default PL/SQL code
+If you use the default PL/SQL code provided with this plugin, the files are saved in a APEX collection called "DROPZONE_UPLOAD". Select it like that:
+
+```language-sql
+SELECT c001    AS filename,
+       c002    AS mime_type,
+       d001    AS date_created,
+       n001    AS file_id,
+       blob001 AS file_content
+  FROM apex_collections
+ WHERE collection_name = 'DROPZONE_UPLOAD';
+ ```
+
+## Hint for ORDS and Tomcat users
+If you have problems with uploading larger files, then it could be a issue with the max. allowed post size of Tomcat server (default is 2MB). To get around this issue please add the parameter **maxPostSize** with a byte value to your connector in server.xml file of tomcat.
+
+For AJP connector:
+```
+<Connector port="8009" protocol="AJP/1.3" redirectPort="8443" address="127.0.0.1" maxPostSize="15728640" />
+```
+
+For HTTP connector:
+```
+<Connector port="8080" protocol="HTTP/1.1" connectionTimeout="20000" redirectPort="8443" address="127.0.0.1" maxPostSize="15728640" />
+```
+
+This example sets maxPostSize to 15MB or 15728640 bytes.
+
+## Demo Application
+https://apex.oracle.com/pls/apex/f?p=APEXPLUGIN
+
+## Preview
+![](https://github.com/Dani3lSun/apex-plugin-dropzone/blob/master/preview.gif)
+
+#### Preview Copy&Paste
+![](https://github.com/Dani3lSun/apex-plugin-dropzone/blob/master/preview_copy_paste.gif)
+---
---
+++ source/render_region.sql
@@ -0,0 +1,210 @@
+/*-------------------------------------
+ * Dropzone Apex Plugin
+ * Version: 1.9.4 (31.08.2016)
+ * Author:  Daniel Hochleitner
+ *-------------------------------------
+*/
+FUNCTION render_dropzone(p_region              IN apex_plugin.t_region,
+                         p_plugin              IN apex_plugin.t_plugin,
+                         p_is_printer_friendly IN BOOLEAN)
+  RETURN apex_plugin.t_region_render_result IS
+  -- plugin attributes
+  l_width                 VARCHAR2(50) := p_region.attribute_01;
+  l_height                VARCHAR2(50) := p_region.attribute_02;
+  l_display_message       VARCHAR2(500) := p_region.attribute_03;
+  l_max_filesize_mb       NUMBER := p_region.attribute_04;
+  l_dz_clickable          VARCHAR2(50) := p_region.attribute_05;
+  l_items_submit          VARCHAR(1000) := p_region.attribute_06;
+  l_plsql                 p_region.attribute_07%TYPE := p_region.attribute_07;
+  l_logging               VARCHAR(50) := p_region.attribute_08;
+  l_remove_uploaded_files VARCHAR(50) := p_region.attribute_09;
+  l_accepted_files        VARCHAR(1000) := p_region.attribute_10;
+  l_filetoobig_message    VARCHAR(500) := p_region.attribute_11;
+  l_maxfiles_message      VARCHAR(500) := p_region.attribute_12;
+  l_max_files             NUMBER := p_region.attribute_13;
+  l_refresh_regionid      VARCHAR(100) := p_region.attribute_14;
+  l_copy_paste_support    VARCHAR(50) := p_region.attribute_15;
+  l_wait_time_ms          NUMBER := p_region.attribute_16;
+  l_parallel_uploads      NUMBER := p_region.attribute_17;
+  l_common_file_preview   VARCHAR(50) := p_region.attribute_18;
+  l_dz_style              VARCHAR2(100) := p_region.attribute_19;
+  l_callback_event        VARCHAR(50) := p_region.attribute_20;
+  l_callback_fnc          VARCHAR(4000) := p_region.attribute_21;
+  -- other variables
+  l_region_id              VARCHAR2(200);
+  l_width_esc              VARCHAR2(50);
+  l_height_esc             VARCHAR2(50);
+  l_display_message_esc    VARCHAR2(500);
+  l_filetoobig_message_esc VARCHAR2(500);
+  l_maxfiles_message_esc   VARCHAR2(500);
+  l_refresh_regionid_esc   VARCHAR(100);
+  -- js/css file vars
+  l_apexdropzone_js VARCHAR2(50);
+  l_dropzone_js     VARCHAR2(50);
+  l_dropzone_css    VARCHAR2(50);
+  l_filereader_js   VARCHAR2(50);
+  --
+BEGIN
+  -- Debug
+  IF apex_application.g_debug THEN
+    apex_plugin_util.debug_region(p_plugin => p_plugin,
+                                  p_region => p_region);
+    -- set js/css filenames
+    l_apexdropzone_js := 'apexdropzone';
+    l_dropzone_js     := 'dropzone';
+    l_dropzone_css    := 'dropzone';
+    l_filereader_js   := 'filereader';
+  ELSE
+    l_apexdropzone_js := 'apexdropzone.min';
+    l_dropzone_js     := 'dropzone.min';
+    l_dropzone_css    := 'dropzone.min';
+    l_filereader_js   := 'filereader.min';
+  END IF;
+  -- set variables and defaults
+  l_region_id             := apex_escape.html_attribute(p_region.static_id ||
+                                                        '_dropzone');
+  l_dz_style              := nvl(l_dz_style,
+                                 'STYLE1');
+  l_max_filesize_mb       := nvl(l_max_filesize_mb,
+                                 2);
+  l_dz_clickable          := nvl(l_dz_clickable,
+                                 'true');
+  l_remove_uploaded_files := nvl(l_remove_uploaded_files,
+                                 'false');
+  l_max_files             := nvl(l_max_files,
+                                 256);
+  l_copy_paste_support    := nvl(l_copy_paste_support,
+                                 'false');
+  l_wait_time_ms          := nvl(l_wait_time_ms,
+                                 600);
+  l_parallel_uploads      := nvl(l_parallel_uploads,
+                                 1);
+  IF l_parallel_uploads > 2 THEN
+    l_parallel_uploads := 2;
+  ELSIF l_parallel_uploads = 0 THEN
+    l_parallel_uploads := 1;
+  END IF;
+  l_common_file_preview := nvl(l_common_file_preview,
+                               'true');
+  l_filetoobig_message  := nvl(l_filetoobig_message,
+                               'File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.');
+  l_maxfiles_message    := nvl(l_maxfiles_message,
+                               'You can not upload more than {{maxFiles}} files.');
+  l_logging             := nvl(l_logging,
+                               'false');
+  -- escape input
+  l_width_esc              := sys.htf.escape_sc(l_width);
+  l_height_esc             := sys.htf.escape_sc(l_height);
+  l_display_message_esc    := sys.htf.escape_sc(l_display_message);
+  l_filetoobig_message_esc := sys.htf.escape_sc(l_filetoobig_message);
+  l_maxfiles_message_esc   := sys.htf.escape_sc(l_maxfiles_message);
+  l_refresh_regionid_esc   := sys.htf.escape_sc(l_refresh_regionid);
+  --
+  -- add div for dropzone
+  -- style 1 (grey border)
+  IF l_dz_style = 'STYLE1' THEN
+    sys.htp.p('<div id="' || l_region_id ||
+              '" class="dropzone" style="border:5px solid grey;background: white;width:' ||
+              l_width_esc || ';height:' || l_height_esc ||
+              ';overflow:auto;"></div>');
+    -- style 2 (blue dashed border)
+  ELSIF l_dz_style = 'STYLE2' THEN
+    sys.htp.p('<div id="' || l_region_id ||
+              '" class="dropzone" style="border: 3px dashed #0087F7;border-radius: 5px;background: white;width:' ||
+              l_width_esc || ';height:' || l_height_esc ||
+              ';overflow:auto;"></div>');
+  END IF;
+  --
+  -- add dropzone js and apexdropzone
+  apex_javascript.add_library(p_name           => l_dropzone_js,
+                              p_directory      => p_plugin.file_prefix ||
+                                                  'js/',
+                              p_version        => NULL,
+                              p_skip_extension => FALSE);
+  --
+  apex_javascript.add_library(p_name           => l_apexdropzone_js,
+                              p_directory      => p_plugin.file_prefix ||
+                                                  'js/',
+                              p_version        => NULL,
+                              p_skip_extension => FALSE);
+  -- filereader for Copy&Paste support
+  IF l_copy_paste_support = 'true' THEN
+    apex_javascript.add_library(p_name           => l_filereader_js,
+                                p_directory      => p_plugin.file_prefix ||
+                                                    'js/',
+                                p_version        => NULL,
+                                p_skip_extension => FALSE);
+  END IF;
+  --
+  -- add dropzone css
+  apex_css.add_file(p_name      => l_dropzone_css,
+                    p_directory => p_plugin.file_prefix || 'css/');
+  --
+  -- onload code
+  apex_javascript.add_onload_code(p_code => 'apexDropzone.apexDropzoneFnc(' ||
+                                            apex_javascript.add_value(p_region.static_id) || '{' ||
+                                            apex_javascript.add_attribute('pageItems',
+                                                                          apex_plugin_util.page_item_names_to_jquery(l_items_submit)) ||
+                                            apex_javascript.add_attribute('ajaxIdentifier',
+                                                                          apex_plugin.get_ajax_identifier) ||
+                                            apex_javascript.add_attribute('maxFilesize',
+                                                                          l_max_filesize_mb) ||
+                                            apex_javascript.add_attribute('clickable',
+                                                                          l_dz_clickable) ||
+                                            apex_javascript.add_attribute('removeAfterUpload',
+                                                                          l_remove_uploaded_files) ||
+                                            apex_javascript.add_attribute('maxFiles',
+                                                                          l_max_files) ||
+                                            apex_javascript.add_attribute('acceptedFiles',
+                                                                          l_accepted_files) ||
+                                            apex_javascript.add_attribute('refreshRegionID',
+                                                                          l_refresh_regionid_esc) ||
+                                            apex_javascript.add_attribute('supportCopyPaste',
+                                                                          l_copy_paste_support) ||
+                                            apex_javascript.add_attribute('waitTime',
+                                                                          l_wait_time_ms) ||
+                                            apex_javascript.add_attribute('parallelUploads',
+                                                                          l_parallel_uploads) ||
+                                            apex_javascript.add_attribute('commonFilePreview',
+                                                                          l_common_file_preview) ||
+                                            apex_javascript.add_attribute('callbackEvent',
+                                                                          l_callback_event) ||
+                                            apex_javascript.add_attribute('callbackFnc',
+                                                                          l_callback_fnc) ||
+                                            apex_javascript.add_attribute('pluginPrefix',
+                                                                          p_plugin.file_prefix) ||
+                                            apex_javascript.add_attribute('defaultMessage',
+                                                                          l_display_message_esc) ||
+                                            apex_javascript.add_attribute('fileTooBigMessage',
+                                                                          l_filetoobig_message_esc) ||
+                                            apex_javascript.add_attribute('maxFilesMessage',
+                                                                          l_maxfiles_message_esc,
+                                                                          FALSE,
+                                                                          FALSE) || '},' ||
+                                            apex_javascript.add_value(l_logging,
+                                                                      FALSE) || ');');
+  --
+  RETURN NULL;
+  --
+END render_dropzone;
+--
+--
+-- AJAX function
+--
+--
+FUNCTION ajax_dropzone(p_region IN apex_plugin.t_region,
+                       p_plugin IN apex_plugin.t_plugin)
+  RETURN apex_plugin.t_region_ajax_result IS
+  --
+  -- plugin attributes
+  l_result apex_plugin.t_region_ajax_result;
+  l_plsql  p_region.attribute_07%TYPE := p_region.attribute_07;
+  --
+BEGIN
+  -- execute PL/SQL
+  apex_plugin_util.execute_plsql_code(p_plsql_code => l_plsql);
+  --
+  --
+  RETURN NULL;
+  --
+END ajax_dropzone;
